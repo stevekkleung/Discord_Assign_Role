3a71c44c80f847a1be2fe48d4f709c3cce00680d
Steve <stevekkleung@gmail.com>
2023-01-07 18:31:32 -0800

    Update prod.py
    version update:
    bot execute once every 12 hours, no ctx commands needed
    remove role function disabled
    
    serverId needs to update if use at other servers

--- prod.py
+++ prod.py
@@ -1,89 +1,127 @@
+#######################################################################
+# BonFire Discord Bot: assign_role
+# Purpose: Read-in BonFire API: discord-role-assign
+#          1) Assign Property-Roles to userDiscordId
+#          2) Remove Roles if not in API (currently disabled)
+######################################################################
+# Bot triggers once every 12 hours
+######################################################################
+
 # required packages
 import os
 from dotenv import load_dotenv
 
-# required for discord bots
+import requests
+import json
 import discord
-#import asyncio
-#from discord import app_commands
-from discord.ext import commands
+import asyncio
+# from discord import app_commands
+from discord.ext import commands, tasks
 
-# required hotfix
+# required hotfix for
 import nest_asyncio
 nest_asyncio.apply()
 
-# parameters for bot
-INPUT_TEXT = 'names_and_roles.txt'
+# API URL
+url = "https://app.bonfire.capital/api/discord-role-assign"  
 
-# bot token
+# read discord bot token
 load_dotenv()
 BOT_TOKEN = os.getenv("TOKEN_assign_role")
 
+# Discord Server ID
+serverId = 1038544563410841630 #testing server
+#serverId = 986758425394425959  #production server
+
+# Discord bot required commands
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 bot = commands.Bot(command_prefix='?', intents=intents)
 
+
+# bot online message
 @bot.event
 async def on_ready():
-    print(f'{bot.user} is running!')
+    print('------------------------')
+    print(f'{bot.user} is now running! (BOT ID: {bot.user.id}) \n')
+    role_loop.start()
 
-@bot.command()
-async def role(ctx):
-    # find all unique roles from txt
-    roles = set()
-    with open(INPUT_TEXT, 'r') as f:
-        next(f)
-        lines = f.readlines()
-        for line in lines:
-            roles.add(line.split(',')[1].strip())
-    f.close()
+#@tasks.loop(seconds=30)
+@tasks.loop(hours=12)
+async def role_loop():
+    await bot.assign_role()
 
-    # loop all roles
-    for role in roles:
-        # list of names for each role
-        names = set()
-        with open(INPUT_TEXT,'r') as f:
-            next(f)
-            lines = f.readlines()
-            for line in lines:
-                if line.split(',')[1].strip()==role:
-                    names.add(line.split(',')[0].strip())
-        f.close()
+# assign discordIds to discordRoles based on the API
+@bot.event
+async def assign_role():
+    # define guild with server ID
+    guild = bot.get_guild(serverId)
 
-        # convert name from text to object
+    # read API and create a json file
+    API_json = json.loads(requests.get(url).text)    
+    
+    # find all the unique discord roles from API
+    discordRoles = set()
+    for i in range(len(API_json)):
+        if API_json[i]['discordRole']!=None:
+            discordRoles.add(API_json[i]['discordRole'])
+    print(f'checkpoint 1: print unique discordRoles from API: {discordRoles}')
+
+    # loop through all of the discordRoles
+    # within the API, find all of the discordIds for each of the roles
+    for discordRole in discordRoles:
+        userDiscordIds = set()
+        for i in range(len(API_json)):
+            if API_json[i]['discordRole']==discordRole and API_json[i]['userDiscordId']!=None:
+                userDiscordIds.add(API_json[i]['userDiscordId'])
+        print(f'checkpoint 2: for discordRole: {discordRole}, we have these discordIds: {userDiscordIds}')
+
+        # convert discord name into member object, then put them into a list
         members = set()
-        converter = commands.MemberConverter()
-        for name in names:
+        for userDiscordId in userDiscordIds:
             try:
-                member = await converter.convert(ctx, name)
+                member = guild.get_member_named(userDiscordId)
                 members.add(member)
+                print(f'checkpoint 2b: userDiscordId is: {userDiscordId} & member object is {member}')
+#            print(f'checkpoint 2b: {userDiscordId} & {member} data type updated')
             except:
+                print(f'checkpoint 2c: {userDiscordId} exception raised')
                 pass
+        print(f'checkpoint 3: userDiscordIds value: {members}')
 
-        # convert role from text to object
-        role = discord.utils.get(ctx.guild.roles, name=role)
+        # convert role name into role id
+        role = discord.utils.get(guild.roles, name=discordRole)
+        roleid = role.id
+        print(f'checkpoint 4: role: {role} and roleid: {roleid}')
 
-        # find names with the role
+        # find all existing memberids in the server with the role
         existing_members = set()
         for i in role.members:
             existing_members.add(i)
 
         add_role_list = list(members - existing_members)
-        remove_role_list = list(existing_members - members)
-
-        # add role
+        # loop the list and add roles
         for i in add_role_list:
+            print(f'checkpoint 5: member value: {i}')
             try:
                 await i.add_roles(role)
+                print(f'checkpoint 5b: added role {role} to {i}')
             except:
+                print('checkpoint 5c: error')
                 pass
 
-        # remove role
+'''
+        remove_role_list = list(existing_members - members)
+        # loop the list and remove roles
         for i in remove_role_list:
+            print(f'checkpoint 5: member value: {i}')
             try:
                 await i.remove_roles(role)
+                print(f'checkpoint 5b: removed role {role} from {i}')
             except:
+                print('checkpoint 5c: error')
                 pass
+'''
 
 bot.run(BOT_TOKEN)
